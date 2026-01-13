ce5933690f1ec202311e12828c829a8bd11c7bf2
kishoraman21 <kishoraman2121@gmail.com>
2026-01-14 01:19:18 +0530

    add counts to fetch

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -7,7 +7,7 @@ export async function GET() {
 
     const products = await product
       .find({ isActive: true })
-      .select("title category price description pages outcomes contents")
+      .select("title category price description pages outcomes contents counts")
       .sort({ createdAt: -1 });
 
     return Response.json(
