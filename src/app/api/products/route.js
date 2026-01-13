bfc1456e47cc8c758440729237d558cf594a4df8
kishoraman21 <kishoraman2121@gmail.com>
2026-01-13 20:33:16 +0530

    selected the contents , description , pages and outcomes

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -7,7 +7,7 @@ export async function GET() {
 
     const products = await product
       .find({ isActive: true })
-      .select("title category price")
+      .select("title category price description pages outcomes contents")
       .sort({ createdAt: -1 });
 
     return Response.json(
